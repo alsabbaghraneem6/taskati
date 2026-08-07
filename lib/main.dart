1b6901918fb8d5b3afe02b4a8be7d401bd32cdfc
Raneem Alsabbagh <alsabbaghraneem6@gmail.com>
2026-08-06 23:32:23 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_app/My_app.dart';
+import 'home_screen.dart';
+
+
+void main() {
+  runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+      home: BusinessCard(),
+  ),
+  );
+}
+
+
+
